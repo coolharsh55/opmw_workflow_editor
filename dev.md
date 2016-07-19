80fc159f5a8f45757782b43e0a1ca6ca22eef697
Harshvardhan Pandit <coolharsh55@gmail.com>
2016-07-19 15:26:11 +0100

    new:attach data/param to experiment
    The data / param variables are now associated with the experiment. Form is saved through `form_save`,  the elements associated via property or relations are appropriately linked to the object instance.
    
    Signed-off-by: Harshvardhan Pandit <coolharsh55@gmail.com>

--- dev.md
+++ dev.md
@@ -1,46 +1,17 @@
 # dev.md
 
 ## things to do
+ - add step to form
+ - pre-choose selected element in select inputs upon element reload
+ - multi-select
+ - restore textarea contents upon element reload
+ - multiple text inputs for input dimension="multi"
+ - check: form object does not remain the same after changing forms
+ - update label for node
 
-### adding elements to stack
- - `opmw_elements` is the ontology loaded from the json file
- - `experiment_data` is the elements in the experiment
- - the element is added to the stack after validation in form
- - the starting element is _opmw:WorkflowTemplate_ which is the experiment
- - once that is validated, it is added to experiment_data
-#### experiment data
- - the elements are segregated by types variables, steps, experiment template
-```
-form_element = current element in form
-
-onDocumentReady:
-    make_form(
-        type=opmw:WorkflowTemplate,  // type of element from opmw
-        id=null)  // null id means this is a new element
-
-make_form(element_type, id):
-    clear_form_elements()
-    element = opmw_elements.element_type
-    form.header = element.label
-    for property in element.properties:
-        form.properties.add(
-            property.input,  // type of input
-            property.label,  // label to display
-            property.required  // whether this is a required input
-        )
-    for relation in element.relations:
-        label = relation.info
-        for related_element in experiment_data[relation.domain]:
-            if related_element.properties[relation.property] == this:
-                form.relations.add(related_element.label)
-
-add_element_btn():
-    check_if_form_is_validated()
-    save_form_element()
-```
-### re-edit ontology - add missing parts and bits
-### establishing relation between domain-range for elements
 ### attaching element to diagram
+ - create new diagram for data, param, step
+
 ### moving between elements
 ### arranging elements in diagram
 ### exporting ontology
--- form.js
+++ form.js
@@ -30,6 +30,67 @@ var form_data = {
 
 
 /**
+ * save form
+ *
+ * Will save the form data into the element data
+ * and add it to experiment_data under appropriate element type.
+ *
+ * @return {boolean} returns save status
+ */
+var form_save = function() {
+    // validate form before saving
+    if (!form_validate()) {
+        console.log("save failed: form is not validated");
+        return false;
+    }
+
+    // check if there is a duplicate ID
+    if (form_data.object["rdfs:label"] in experiment_data_labels) {
+        if (experiment_data_labels[form_data.object["rdfs:label"]] != form_data.object) {
+            alert('label already in use');
+            return false;
+        }
+    }
+
+    // add form data to element
+    if (!form_add_element_data()) {
+        console.log("save failed: could not add form data to element");
+    }
+
+    // add form element to element_data
+    if (form_data.object.id == null) {
+        experiment_data[form_data.type].push(form_data.object);
+        experiment_data_labels[form_data.object["rdfs:label"]] = form_data.object;
+        form_data.object.id = 0;
+        // attach element to tree
+        if (form_data.type == "opmw:WorkflowTemplate") {
+            $('#tree-experiment').text(form_data.object["rdfs:label"]);
+        } else if (form_data.type == "opmw:WorkflowTemplateProcess") {
+            $('#steps ul').append('<li>' + form_data.object["rdfs:label"]);
+        } else if (form_data.type == "opmw:DataVariable") {
+            $('#variables_data ul').append('<li>' + form_data.object["rdfs:label"]);
+        } else if (form_data.type == "opmw:ParameterVariable") {
+            $('#variables_param ul').append('<li>' + form_data.object["rdfs:label"]);
+        }
+    }
+    console.log(experiment_data);
+
+    // update label in tree
+    // if (form_data.type == "opmw:WorkflowTemplate") {
+    //     $('#tree-experiment').text(form_data.object["rdfs:label"]);
+    // } else if (form_data.type == "opmw:WorkflowTemplateProcess") {
+    //     $('#steps ul').append('<li>' + form_data.object["rdfs:label"]);
+    // } else if (form_data.type == "opmw:DataVariable") {
+    //     $('#variables_data ul').append('<li>' + form_data.object["rdfs:label"]);
+    // } else if (form_data.type == "opmw:ParameterVariable") {
+    //     $('#variables_param ul').append('<li>' + form_data.object["rdfs:label"]);
+    // }
+
+    return true;
+}
+
+
+/**
  * validate form
  *
  * Will validate the form and return status
@@ -63,14 +124,49 @@ var form_validate = function() {
  * @return {boolean} operation status
  */
 var form_add_element_data = function() {
-    // TODO: (function) form add element data
-    // if form_data.element === null, this is a new element
-    var element = {
-        "type": form_data.type,
-        "schema": form_data.schema,
-        "properties": {},
-        "relations": {}
-    }
+
+    // TODO: add form data to form element
+    Object.keys(form_data.schema.properties).forEach(function(key, index) {
+        console.log(
+            key,
+            form_data.schema.properties[key],
+            $("input[name='" + key + "']").val(),
+            key in form_data.object);
+        var field_type = form_data.schema.properties[key].input;
+        if (field_type == "text") {
+            // create array if type is list
+            if (form_data.schema.properties[key].dimension == "multi" ||
+                    form_data.schema.properties[key].dimension > 1) {
+                // TODO: add values to array
+                form_data.object[key] = [$("input[name='" + key + "']").val()];
+            } else {
+                form_data.object[key] = $("input[name='" + key + "']").val();
+            }
+        } else if (field_type == "select") {
+            // create object link if type is select
+            // see if select has a value
+            if ($("#" + key.replace(':', '-')).length > 0) {
+                // get label of selected element
+                var label_related_obj =
+                    $("#" + key.replace(':', '-') + " option:selected").text();
+                console.log("select value", label_related_obj);
+                // get related element object
+                var related_obj = experiment_data_labels[label_related_obj];
+                // check if element object exists (it should!)
+                if (related_obj == null) {
+                    console.log("error finding related object with label", label_related_obj);
+                } else {
+
+                    form_data.object[key] = related_obj;
+                    related_obj.links[key].push(form_data.object);
+                }
+            }
+        }
+    });
+    console.log("updated form_data.object", form_data.object);
+
+    // TODO: property and relation resolution
+    // NOTE:
     // forEach(property in form_data.element.properties) {
     //     get property from form
     //     element.properties.attach(property)
@@ -81,7 +177,7 @@ var form_add_element_data = function() {
     //     element.relations.attach(relations)
     //     link back to relation if necessary
     // }
-    form_data.object = element;
+
     return true;
 }
 
@@ -111,6 +207,7 @@ var form_add_property = function(field_name, field) {
         var input = $("<input>", {
             field_type: 'text',
             name: field_name,
+            value: form_data.object[field_name]
         });
         if (field.required) {
             input.prop('required', true);
@@ -118,12 +215,16 @@ var form_add_property = function(field_name, field) {
         label.append(input);
     // input is a select field
     } else if (field_type == "select") {
-        var select = $('<select>');
-        for (var i=0; i<options.length; ++i) {
-            select.append($('<option>', {
-                attr: {value: i},
-                html: i
-            }));
+        // TODO: restore field value
+        var select = $('<select>', {id: field_name.replace(':', '-')});
+        console.log(field.range, experiment_data[field.range]);
+        if (experiment_data[field.range] != null) {
+            experiment_data[field.range].forEach(function(field_obj, index) {
+                select.append($('<option>', {
+                    attr: {value: field_obj["rdfs:label"]},
+                    html: field_obj["rdfs:label"]
+                }));
+            })
         }
         if (field.required) {
             select.prop('required', true);
@@ -132,10 +233,12 @@ var form_add_property = function(field_name, field) {
     // input is a multi-select field
     // TODO: add multi-select field type to form
     } else if (field_type == "multi-select") {
+        // TODO: restore field value
         label.append('test');
     // input is a text area
     } else {
-        label.append($("<textarea>"));
+        // TODO: restore textarea value
+        label.append($("<textarea>"), {value: form_data.object[field_name]});
     }
     // attach label and input to infobox
     $('#form-properties').append(label);
@@ -149,12 +252,31 @@ var form_add_property = function(field_name, field) {
  * @return {nothing}
  */
 var form_make = function(type, schema, object=null) {
+    console.log("creating form for", type, object);
     // TODO: check, validate, save previous form data before making new one
+    // NOTE: this should not be done in this function (?)
+    //      the form data save status could be added to the form element
+    //      and if it is saved, then just creat the new form
+    //      otherwise save and exit? or ask if they want to lose the data?
 
     // object is the instance of that element
     // if it is null, then we create a new element
-    if (object != null) {
-        // TODO: create new element for schema
+    if (object == null) {
+        // create new element for schema
+        console.log("element schema", schema);
+        object = {id: null, schema: schema, type: type, links: {}};
+        Object.keys(schema.properties).forEach(function(key, index) {
+            var property = schema.properties[key];
+            if (property.dimension == "multi") {
+                object[key] = [];
+            } else {
+                object[key] = null;
+            }
+        });
+        Object.keys(schema.relations).forEach(function(key, index) {
+            object.links[key] = [];
+        });
+        console.log("new schema object", object);
     }
     // add details to form object
     form_data.type = type;
@@ -176,19 +298,19 @@ var form_make = function(type, schema, object=null) {
         form_add_property(key, property);
     });
 
-    // create elements for each relation
-    Object.keys(schema.relations).forEach(function(key, index) {
-        // add related relation objects
-        var relation = schema.relations[key];
-        if(relation.domain in experiment_data) {
-            Object.keys(experiment_data[relation.domain]).forEach(function(key2, index) {
-                var related_object = experiment_data.key.key2;
-                if (relation.domain == key2) {
-                    // TODO: check object is in related_object.properties and add as related
-                }
+    // show relations only if the object has an id
+    if (object.id != null) {
+        // create elements for each relation
+        Object.keys(object.links).forEach(function(key, index) {
+            var related_links = $('<ul>');
+            object.links[key].forEach(function(ele, index) {
+                related_links.append($('<li>', {text: ele["rdfs:label"]}));
             });
-        }
-    });
+            console.log(related_links);
+            $('#form-relations').append(
+                $('<li>', {text: schema.relations[key].label}).append(related_links));
+        });
+    }
 
     return true;
 }
