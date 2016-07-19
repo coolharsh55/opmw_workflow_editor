51a3e63adb9f1a12126109ad3c349c16a7f6579f
Harshvardhan Pandit <coolharsh55@gmail.com>
2016-07-19 15:23:05 +0100

    new:tree.js onclick events for adding elements
    The onClick handlers have been added to data, param, and step to add them to the diagram. Clicking on the experiment label will now open the experiment element if it has not already been declared.
    
    Signed-off-by: Harshvardhan Pandit <coolharsh55@gmail.com>

--- tree.js
+++ tree.js
@@ -1,34 +1,55 @@
 /*
-    tree.js
-    tree (right panel) functions and globals
+ tree.js
+ tree (right panel) functions and globals
 
-    The tree shows experiment data objects -
-    data variables, parameters and steps.
+ The tree shows experiment data objects -
+ data variables, parameters and steps.
 
-    @author: Harshvardhan Pandit
-    @email : me@harshp.com
+ @author: Harshvardhan Pandit
+ @email : me@harshp.com
  */
 
 
 $('document').ready(function() {
-    // on click event handler for + button in Data variables
-    $('#add-data-var').click(function() {
-        form_validate();
-    });
-    // on click event handler for + button in Parameter variables
-    $('#add-param-var').click(function() {
-        form_validate();
-    });
-    // on click event handler for + button in Step
-    $('#add-step-var').click(function() {
-        form_validate();
-    });
-    // allow expand/collapse of tree nodes
-    $('.list > h5').click(function() {
-        $(this).parent().find('ul').toggle();
-    });
-    // TODO: (remove/testing) tree experiment make form
-    $('#tree-experiment').click(function() {
-        make_form(experiment);
-    });
+
+});
+
+// on click event handler for + button in Data variables
+$('#add-data-var').click(function() {
+    form_make(
+        "opmw:DataVariable",
+        OPMW.elements["opmw:DataVariable"], null);
+});
+// on click event handler for + button in Parameter variables
+$('#add-param-var').click(function() {
+    form_make(
+        "opmw:ParameterVariable",
+        OPMW.elements["opmw:ParameterVariable"], null);
+});
+// on click event handler for + button in Step
+$('#add-step-var').click(function() {
+    form_make(
+        "opmw:WorkflowTemplateProcess",
+        OPMW.elements["opmw:WorkflowTemplateProcess"], null);
+});
+// allow expand/collapse of tree nodes
+$('.list > h5').click(function() {
+    $(this).parent().find('ul').toggle();
+});
+
+
+/*
+ click events for tree nodes
+ */
+
+// experiment
+$('#tree-experiment').click(function() {
+    if (experiment_data["opmw:WorkflowTemplate"].length != 0) {
+        form_make(
+            "opmw:WorkflowTemplate",
+            OPMW.elements["opmw:WorkflowTemplate"],
+            experiment_data["opmw:WorkflowTemplate"][0]);
+    } else {
+        // do nothing
+    }
 });
