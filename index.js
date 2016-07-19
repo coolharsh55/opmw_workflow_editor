823dec6a62c0cc95f56f943f01159ab76b67604d
Harshvardhan Pandit <coolharsh55@gmail.com>
2016-07-19 15:21:20 +0100

    change:experiment_data is now an array, not dict
    The `experiment_data[type]` collection for elements was changed from dictionary to list since a dictionary neccesitates that the key be known or all values should be traveresed to get the values. The list holds all objects of a given type, with the label lookup handled by the new var `experiment_data_labels`.
    
    Signed-off-by: Harshvardhan Pandit <coolharsh55@gmail.com>

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ var OPMW = null;
 // experiment data holds the experiment data produced in the workflow editor
 // is a dictionary by types
 var experiment_data = {};
+// `experiment data labels` holds references to the various elements
+// instantiated which can be accessed using their labels
+// It also holds references in the tree and in the diagram
+var experiment_data_labels = {};
 
 /*
     on document ready
@@ -26,19 +30,27 @@ $('document').ready(function() {
         add element types to experiment data
         make form for experiment template
      */
-    $.getJSON("http://lvh.me:8000/opmw.json", function(json) {
+    $.getJSON("opmw.json", function(json) {
         OPMW = json;
         // for debug purposes, ALWAYS log the OPMW to console
         console.log("opmw.json", OPMW);
         // TODO: set up types for experiment data
         // add each type from OPMW.elements to experiment_data
         Object.keys(OPMW.elements).forEach(function(type, index) {
-            experiment_data[type] = {};
+            experiment_data[type] = [];
         });
+        console.log("experiment data", experiment_data);
 
         // make form for experiment
         form_make(
             OPMW.begin,         // element type
             OPMW.elements[OPMW.begin]);  // element schema);
     });
+});
+
+$('#form-save').click(function() {
+    form_save();
+});
+$('#form-cancel').click(function() {
+    form_cancel();
 });
